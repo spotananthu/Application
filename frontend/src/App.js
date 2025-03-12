681149ea97e7e8888c486a4c2ba246b46fa64491
Anantha Krishnan <ananthutemp@gmail.com>
2025-03-12 21:41:09 +0530

    added dockerfiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:4000/tasks";
+const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 function App() {
     const [tasks, setTasks] = useState([]);
