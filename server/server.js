a3dee355d3d224dad8a3c2aa804be0032b1f430c
Anantha Krishnan <ananthutemp@gmail.com>
2025-03-12 20:56:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
